teeworlds/teeworlds

math: Rename frandom() to random_float() to match with random_int()

src/game/client/components/damageind.cpp
@@ -40,7 +40,7 @@ void CDamageInd::Create(vec2 Pos, vec2 Dir)
 		pItem->m_Pos = Pos;
 		pItem->m_LifeTime = 0.75f;
 		pItem->m_Dir = Dir*-1;
-		pItem->m_StartAngle = (frandom() - 1.0f) * 2.0f * pi;
+		pItem->m_StartAngle = (random_float() - 1.0f) * 2.0f * pi;
 	}
 }
 

nonosch07/Blockworlds

(cherry picked from commit https://github.com/teeworlds/teeworlds/commit/5e4c7b66c555fa163ea46b960bbe7f5aed953ad1)

src/game/client/components/damageind.cpp
@@ -42,7 +42,7 @@ void CDamageInd::Create(vec2 Pos, vec2 Dir)
 		i->m_Pos = Pos;
 		i->m_StartTime = LocalTime();
 		i->m_Dir = Dir * -1;
-		i->m_StartAngle = (((float)rand() / (float)RAND_MAX) - 1.0f) * 2.0f * pi;
+		i->m_StartAngle = (random_float() - 1.0f) * 2.0f * pi;
 	}
 }
 