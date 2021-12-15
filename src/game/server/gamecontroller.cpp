teeworlds/teeworlds

add IServer::ChangeMap to fix change_map command

src/game/server/gamecontroller.cpp
@@ -932,7 +932,8 @@ void IGameController::CycleMap()
 		char aBuf[256];
 		str_format(aBuf, sizeof(aBuf), "rotating map to %s", m_aMapWish);
 		GameServer()->Console()->Print(IConsole::OUTPUT_LEVEL_DEBUG, "game", aBuf);
-		str_copy(Config()->m_SvMap, m_aMapWish, sizeof(Config()->m_SvMap));
+		Server()->ChangeMap(m_aMapWish);
+
 		m_aMapWish[0] = 0;
 		m_MatchCount = 0;
 		return;
@@ -991,7 +992,7 @@ void IGameController::CycleMap()
 	char aBufMsg[256];
 	str_format(aBufMsg, sizeof(aBufMsg), "rotating map to %s", &aBuf[i]);
 	GameServer()->Console()->Print(IConsole::OUTPUT_LEVEL_DEBUG, "game", aBuf);
-	str_copy(Config()->m_SvMap, &aBuf[i], sizeof(Config()->m_SvMap));
+	Server()->ChangeMap(&aBuf[i]);
 }
 
 // spawn

nonosch07/Blockworlds

(cherry picked from commit 97d907304c56f643a9ed99e60c35052f950b27fb)

src/game/server/gamecontroller.cpp
@@ -466,7 +466,7 @@ void IGameController::StartRound()
 
 void IGameController::ChangeMap(const char *pToMap)
 {
-	str_copy(g_Config.m_SvMap, pToMap, sizeof(g_Config.m_SvMap));
+	Server()->ChangeMap(pToMap);
 }
 
 void IGameController::OnReset()